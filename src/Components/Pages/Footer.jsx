95ec8b34506449530bf231473707042e3e9d1ebf
Vishal Raj <vr288650@gmail.com>
2025-08-25 09:36:36 +0530

    initial commit

---
+++ src/Components/Pages/Footer.jsx
@@ -0,0 +1,180 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div
+        className="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn"
+        data-wow-delay="0.1s"
+      >
+        <div className="container py-5">
+          <div className="row g-5">
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Quick Link</h4>
+              <a className="btn btn-link" href>
+                About Us
+              </a>
+              <a className="btn btn-link" href>
+                Contact Us
+              </a>
+              <a className="btn btn-link" href>
+                Privacy Policy
+              </a>
+              <a className="btn btn-link" href>
+                Terms &amp; Condition
+              </a>
+              <a className="btn btn-link" href>
+                FAQs &amp; Help
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Contact</h4>
+              <p className="mb-2">
+                <i className="fa fa-map-marker-alt me-3" />
+                Bhopal, Madhya Pradesh, India
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-phone-alt me-3" />
+                +91 9905022100
+              </p>
+              <p className="mb-2">
+                <i className="fa fa-envelope me-3" />
+                vishalsinghrajput99050@gmail.com
+              </p>
+              <div className="d-flex pt-2">
+                <a
+                  className="btn btn-outline-light btn-social"
+                  href="https://www.instagram.com/vishalsingh________?igsh=eWo0MHkxcTJwNA=="
+                  target="_blank"
+                >
+                  <i className="fab fa-instagram" />
+                </a>
+                <a
+                  className="btn btn-outline-light btn-social"
+                  href="https://www.facebook.com/share/199Bb4LLpr/"
+                  target="_blank"
+                >
+                  <i className="fab fa-facebook-f" />
+                </a>
+                <a
+                  className="btn btn-outline-light btn-social"
+                  href="https://github.com/vishalsinghrajput05"
+                  target="_blank"
+                >
+                  <i className="fab fa-github" />
+                </a>
+                <a
+                  className="btn btn-outline-light btn-social"
+                  href="https://www.linkedin.com/in/vishalraj99/"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin-in" />
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Gallery</h4>
+              <div className="row g-2 pt-2">
+                <div className="col-4">
+                  <img
+                    className="img-fluid bg-light p-1"
+                    src="/img/course-1.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="col-4">
+                  <img
+                    className="img-fluid bg-light p-1"
+                    src="/img/course-2.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="col-4">
+                  <img
+                    className="img-fluid bg-light p-1"
+                    src="/img/course-3.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="col-4">
+                  <img
+                    className="img-fluid bg-light p-1"
+                    src="/img/course-2.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="col-4">
+                  <img
+                    className="img-fluid bg-light p-1"
+                    src="/img/course-3.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="col-4">
+                  <img
+                    className="img-fluid bg-light p-1"
+                    src="/img/course-1.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <h4 className="text-white mb-3">Newsletter</h4>
+              <p>
+                This website is a Final Year project of CSE Department OIST,
+                Bhopal.
+              </p>
+              <div
+                className="position-relative mx-auto"
+                style={{ maxWidth: "400px" }}
+              >
+                <input
+                  className="form-control border-0 w-100 py-3 ps-4 pe-5"
+                  type="text"
+                  placeholder="Your email"
+                />
+                <button
+                  type="button"
+                  className="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2"
+                >
+                  SignUp
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="copyright">
+            <div className="row">
+              <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                ©{" "}
+                <a className="border-bottom" href>
+                  eLearning
+                </a>
+                , All Right Reserved. Designed By{" "}
+                <a
+                  className="border-bottom"
+                  href="https://www.linkedin.com/in/vishalraj99/"
+                  target="_blank"
+                >
+                  Vishal Raj
+                </a>
+                <br />
+                <br />
+              </div>
+              <div className="col-md-6 text-center text-md-end">
+                <div className="footer-menu">
+                  <a href>Home</a>
+                  <a href>Cookies</a>
+                  <a href>Help</a>
+                  <a href>FQAs</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
